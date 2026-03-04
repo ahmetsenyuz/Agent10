be7f40083c115855925cb5655c09417e11e9906b
ahmetsenyuz <89253941+ahmetsenyuz@users.noreply.github.com>
2026-03-04 22:15:08 +0300

    BI-0001: Add main program file for Agent10 console application

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Agent10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
